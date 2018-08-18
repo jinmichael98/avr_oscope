f23aaeea92a21e1771177779fb52d0df2c6b3836
jinmichael_98 <jinmichael_98@live.com>
2018-08-17 21:16:16 -0500

    First commit

---
+++ oscope.c
@@ -0,0 +1,6 @@
+#include <avr/io.h>
+#include <avr/iom168.h>
+#include <util/delay.h>
+
+#include "oscope.h"
+
---
+++ oscope.h
@@ -0,0 +1,11 @@
+ /* start Usart module */
+void initUsart();
+
+/* start adc*/
+void initAdc();
+
+/* takes reading off adc, return actual voltage */
+uint8_t read();
+
+/* sends reading to serial by usart */
+void sendToSerial(uint8_t reading);
