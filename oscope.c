8be5aaf3bdcb0d954843babcc67fab4f661dd2cf
jinmichael_98 <jinmichael_98@live.com>
2018-08-19 10:25:51 -0500

    INCOMPLETE: Added oscope.c functions

--- oscope.c
+++ oscope.c
@@ -4,3 +4,34 @@
 
 #include "oscope.h"
 
+ /* start Usart module */
+void initUsart() {
+# /* start Usart module */
+void initUsart();
+
+/* start adc*/
+void initAdc();
+
+/* takes reading off adc, return actual voltage */
+uint8_t read();
+
+/* sends reading to serial by usart */
+void sendToSerial(uint8_t reading);
+if USE_2X
+    ADCSRA
+}
+
+/* start adc*/
+void initAdc() {
+
+}
+
+/* takes reading off adc, return actual voltage */
+uint8_t read() {
+
+}
+
+/* sends reading to serial by usart */
+void sendToSerial(uint8_t reading) {
+
+}
